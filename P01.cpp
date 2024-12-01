11ecc995debe8e21108a4dbe267e99a465c5a0cd
Lasse Deleuran <lasse.deleuran@lego.com>
2024-12-01 06:10:46 +0100

    Added P01.

--- P01.cpp
+++ P01.cpp
@@ -1,62 +1,36 @@
-int ret = 0;
-string x;
-
-string digits[9] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-string numbers[9] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
-
-// For part 1:
-int add() {
-  int i = 0;
-  while(true) {
-    if(x[i] >= '0' && x[i] <= '9') {
-      return x[i]-'0';
-    }
-    i++;
-  }
-}
-
 int main() {
-  while(cin >> x) {
-    cout << x << endl;
-    // Part 2 code:
-    int min = 1000000, max = -1, minDigit, maxDigit;
-    for(int i = 0; i < 9; i++) {
-      size_t f = x.find(digits[i]);
-      if(f != string::npos && f < min) {
-	cout << i+1 << " at " << f << endl;
-	minDigit = i+1;
-	min = (int)f;
-      }
-      f = x.rfind(digits[i]);
-      if(f != string::npos && (int)f > max) {
-	cout << i+1 << " at " << f << endl;
-	maxDigit = i+1;
-	max = (int)f;
-      }
-    }
-    for(int i = 0; i < 9; i++) {
-      size_t f = x.find(numbers[i]);
-      if(f != string::npos && f < min) {
-	cout << i+1 << " at " << f << endl;
-	minDigit = i+1;
-	min = (int)f;
-      }
-      f = x.rfind(numbers[i]);
-      if(f != string::npos && (int)f > max) {
-	cout << i+1 << " at " << f << endl;
-	maxDigit = i+1;
-	max = (int)f;
-      }
-    }
-    ret += 10*minDigit + maxDigit;
+  vector<int> L, R;
+  int l, r;
+  while(cin >> l >> r) {
+    L.push_back(l);
+    R.push_back(r);
+  }
+  sort(L.begin(), L.end());
+  sort(R.begin(), R.end());
 
-    // Part 1 code:
-    //ret += 10*add();
-    //reverse(x.begin(), x.end());
-    //ret += add();
+  int answer1 = 0;
+  FORUI(L.size()) {
+    answer1 += ABS(L[i] - R[i]);
+  }
+  cout << answer1 << endl;
+
+  LL answer2 = 0;
+
+  map<int,int> cnt;
+  FORUI(R.size()) {
+    int x = R[i];
+    if(cnt.find(x) == cnt.end()) {
+      cnt[x] = 0;
+    }
+    cnt[x]++;
+  }
+  FORUI(L.size()) {
+    int x = L[i];
+    if(cnt.find(x) != cnt.end())
+      answer2 += x * cnt[x];
   }
   
-
-  cout << "Result:" << endl << ret << endl;
+  cout << answer2 << endl;
+  
   return 0;
 }
